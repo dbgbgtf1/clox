465adbd0e9a6a629584469bdf97d026b9c95f393
dbgbgtf <dudududuMaxVer@outlook.com>
2025-11-22 11:26:36 +0800

    chapter 14

---
+++ include/value.h
@@ -0,0 +1,21 @@
+#ifndef clox_value_h
+#define clox_value_h
+
+typedef double Value;
+
+typedef struct
+{
+  int count;
+  int capacity;
+  Value *values;
+} ValueArray;
+
+void initValueArray (ValueArray *array);
+
+void freeValueArray (ValueArray *array);
+
+void printValue (Value value);
+
+void writeValueArray (ValueArray *array, Value value);
+
+#endif
